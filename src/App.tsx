7e9ff233f7f97d18de7dc4e1d28ccafba83b8cd9
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-05 12:10:15 +0300

    feat: селекторы

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
 import { Master, Specialization } from './pages';
 import { Navigation } from './components/Navigation';
 import { useAppDispatch } from './store/store';
 import { SpecializationThunk } from './store/specializationSlice/thunk';
 import { MasterThunk } from './store/masterSlice/thunk';
+import { getSpecialization } from './store/specializationSlice/selectors';
 import './app.css';
 
 export const App = () => {
@@ -16,6 +18,9 @@ export const App = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // const masters = useSelector(getMaster);
+  const specializations = useSelector(getSpecialization);
+
   return (
     <div className="app">
       <Navigation />
@@ -23,7 +28,7 @@ export const App = () => {
         <Redirect to="/specializations" />
       </Route>
       <Route exact path="/specializations">
-        <Specialization />
+        <Specialization list={specializations.data} />
       </Route>
       <Route exact path="/masters">
         <Master />
---
+++ src/store/specializationSlice/selectors.ts
@@ -0,0 +1,3 @@
+import { RootState } from '../store';
+
+export const getSpecialization = (state: RootState) => state.specialization;
