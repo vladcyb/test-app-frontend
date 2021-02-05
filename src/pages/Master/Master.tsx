09e1cca5a98d648cbdf63692f0f554b95a04136b
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-05 15:46:58 +0300

    fix: верстка

--- src/pages/Master/Master.tsx
+++ src/pages/Master/Master.tsx
@@ -8,25 +8,23 @@ type PropsType = {
 
 export const Master = ({ masters }: PropsType) => (
   <div className="masters">
-    <div className="masters__table">
-      <div className="masters__tableHeader">
-        <div>id</div>
-        <div>login</div>
-        <div>name</div>
-        <div>surname</div>
-        <div>patronymic</div>
-        <div>specialization</div>
-      </div>
-      {masters.data.map((item) => (
-        <div className="masters__row" key={item.id}>
-          <div>{item.id}</div>
-          <div>{item.login}</div>
-          <div>{item.surname}</div>
-          <div>{item.name}</div>
-          <div>{item.patronymic}</div>
-          <div>{item.Specialization.title}</div>
-        </div>
-      ))}
+    <div className="masters__tableHeader">
+      <div>id</div>
+      <div>login</div>
+      <div>name</div>
+      <div>surname</div>
+      <div>patronymic</div>
+      <div>specialization</div>
     </div>
+    {masters.data.map((item) => (
+      <div className="masters__row" key={item.id}>
+        <div>{item.id}</div>
+        <div>{item.login}</div>
+        <div>{item.surname}</div>
+        <div>{item.name}</div>
+        <div>{item.patronymic}</div>
+        <div>{item.Specialization.title}</div>
+      </div>
+    ))}
   </div>
 );
--- src/pages/Specialization/Specialization.tsx
+++ src/pages/Specialization/Specialization.tsx
@@ -8,17 +8,15 @@ type PropsType = {
 
 export const Specialization = ({ specs }: PropsType) => (
   <div className="specialization">
-    <div className="specialization__table">
-      <div className="specialization__tableHeader">
-        <div>id</div>
-        <div>login</div>
-      </div>
-      {specs.data.map((item) => (
-        <div className="specialization__row" key={item.id}>
-          <div>{item.id}</div>
-          <div>{item.title}</div>
-        </div>
-      ))}
+    <div className="specialization__tableHeader">
+      <div>id</div>
+      <div>login</div>
     </div>
+    {specs.data.map((item) => (
+      <div className="specialization__row" key={item.id}>
+        <div>{item.id}</div>
+        <div>{item.title}</div>
+      </div>
+    ))}
   </div>
 );
